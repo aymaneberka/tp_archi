c088968d036129c6bcf34b334b1f9618b07af15c
aymane <aymaneberka@gmail.com>
2025-11-02 12:00:52 +0100

    tp

--- tp2/bank-app/customer-service/src/main/java/ma/formations/msa/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.formations.msa.repositories;
-
-import ma.formations.msa.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
